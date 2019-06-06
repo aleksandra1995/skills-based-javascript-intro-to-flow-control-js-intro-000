d6a53585608301c9631d76177160eefa6b50db5b
Aleksandra Kosc <aleksandra.kosc@gmail.com>
2019-06-06 20:48:08 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -27,6 +27,12 @@ function ternaryTeenager(age) {
 function switchAge(age) {
   switch (age) {
   case (13):
+    case (14)
+    case (15)
+    case (16)
+    case (17)
+    case (18)
+    case (19)
     console.log("You are a teenager");
     default:
     console.log("You have an age") } }
